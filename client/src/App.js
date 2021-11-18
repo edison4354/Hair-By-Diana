96fb6b988a6c3e4fd81f1fe161358df45eb8e325
Edison Li <47466335+edison4354@users.noreply.github.com>
2021-11-18 15:18:10 -0800

    Revert "Update App.js"
    This reverts commit a8e3f06f84475ed5b14fd92361cee3d0b3f0b35a.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import Home from './views/home';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
